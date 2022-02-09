bfb2fc930ecc3d2104a83cb3573f3908b90e0524
ViNguyenHoang <hoangvi0206@gmail.com>
2022-02-09 07:12:49 +0700

    Vi: merge code

---
+++ draplus/src/pages/BoardList.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface BoardListProps {}
+
+const BoardList: React.FC<BoardListProps> = () => {
+    return <div>BoardList</div>;
+};
+
+export default BoardList;
---
+++ draplus/src/routes/Routes.tsx
@@ -0,0 +1,14 @@
+// Libs
+import * as React from "react";
+import { Switch, Route } from "react-router-dom";
+
+// Pages
+import Board from "../pages/Board";
+import BoardList from "../pages/BoardList";
+import Login from "../pages/Login";
+
+const Routes: React.FC = () => {
+    return <Switch></Switch>;
+};
+
+export default Routes;
