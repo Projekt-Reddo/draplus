f01dea719a7dccc9e0086b9df4c670ba32d71ce9
Phúc Ngô <85348631+phucnnh21@users.noreply.github.com>
2022-02-11 20:53:13 +0700

    Remove Login Component in index.ts (#8)
    * config redux and react router
    
    * fixed conflict

--- draplus/src/index.tsx
+++ draplus/src/index.tsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import LoginButton from "./components/LoginButton";
 
 import { Provider } from "react-redux";
 import store from "./store/store";
-import Login from "pages/Login";
 
 ReactDOM.render(
     <Provider store={store}>
-        {/* <App /> */}
-        <Login />
+        <App />
     </Provider>,
     document.getElementById("root")
 );
