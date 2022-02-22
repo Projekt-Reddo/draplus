399d5d7c07546410f3739e11f21d0ab5f82c66f8
dbdbd9 <62873374+ViNguyenHoang@users.noreply.github.com>
2022-02-22 17:54:50 +0700

    Online People Cursor (#30)
    * Vi: Design Toolbar in Board Page
    
    * Add some function of Left tool bar
    
    * add state initLC to redux
    
    * Add almost done Board SignalR, Add Page Not Found, Route Login with Board Page, add some global state
    
    * Merge code
    
    * Add display shape of user throught signalR, refactor code
    
    * Merge
    
    * Update Route after Login
    
    * Merge code
    
    * Load Shapes were drawn by User to Database
    
    * Fix code
    
    * Merge
    
    * Fix handle logic tool Brush, add User to SignalR
    
    * Fix code
    
    * Code Online People Cursor

--- draplus/src/store/actions/index.ts
+++ draplus/src/store/actions/index.ts
@@ -5,11 +5,14 @@ export const LOGIN = "login";
 export const LOGOUT = "logout";
 export const RECEIVE_MESSAGE = "receiveMessage";
 export const SEND_MESSAGE = "sendMessage";
-export const RECEIVE_SHAPE = "receiveshape";
-export const DRAW_SHAPE = "drawshape";
+export const RECEIVE_SHAPE = "receiveShape";
+export const DRAW_SHAPE = "drawShape";
+export const RECEIVE_MOUSE = "receiveMouse";
+export const SEND_MOUSE = "sendMouse";
 export const INITLC = "lc/init";
 export const JOIN_ROOM = "joinRoom";
 export const LEAVE_ROOM = "leaveRoom";
+export const ONLINE_USERS = "onlineUsers";
 
 export { login };
 export { logout };
--- draplus/src/store/reducers/index.ts
+++ draplus/src/store/reducers/index.ts
@@ -3,12 +3,16 @@ import userReducer from "./user";
 import lcReducer from "./lc";
 import shapeReducer from "./shape";
 import chatReducer from "./chat";
+import mouseReducer from "./mouse";
+import onlineUsers from "./onlineUsers";
 
 const rootReducer = combineReducers({
     user: userReducer,
     initLC: lcReducer,
     shape: shapeReducer,
     chat: chatReducer,
+    mouse: mouseReducer,
+    onlineUsers: onlineUsers,
 });
 
 export default rootReducer;
