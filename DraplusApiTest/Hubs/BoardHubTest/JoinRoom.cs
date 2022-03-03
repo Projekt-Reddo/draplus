01dd6e644b3758553ad151676f90b69474a32e00
Trần Thiện Phú <hostcode0301@gmail.com>
2022-03-03 21:08:31 +0700

    Add board injection; modify DrawShape test method

--- DraplusApiTest/Hubs/BoardHubTest/JoinRoom.cs
+++ DraplusApiTest/Hubs/BoardHubTest/JoinRoom.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Threading.Tasks;
 using DraplusApi.Dtos;
 using DraplusApi.Hubs;
+using DraplusApi.Models;
+using MongoDB.Driver;
 using Moq;
 using NUnit.Framework;
 
@@ -12,10 +15,18 @@ namespace DraplusApiTest.Hubs.BoardHubTest
         public async Task JoinRoom_Board_Success()
         {
             // Arrange
-            var boardHub = new BoardHub(mockConnections.Object, mockBoardRepo.Object, mockMapper.Object, mockUserRepo.Object);
+            var boardHub = new BoardHub(
+                connections: mockConnections.Object,
+                boardRepo: mockBoardRepo.Object,
+                mapper: mockMapper.Object,
+                userRepo: mockUserRepo.Object,
+                shapeList: mockShapeList.Object,
+                noteList: mockNoteList.Object);
             AssignToHubRequiredProperties(boardHub); // Resolve hub dependencies as IClientsProxy...
 
             UserConnection userConnection = new UserConnection { Board = "board1" };
+
+            mockBoardRepo.Setup(board => board.GetByCondition(It.IsAny<FilterDefinition<Board>>())).ReturnsAsync(new Board { Id = "board1" });
 
             // Act
             await boardHub.JoinRoom(userConnection);
