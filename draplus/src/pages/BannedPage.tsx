b96025c75c474a0e352a5ccce9576a87419cfe79
Bao Loc Pham <67360122+BaoLocPham@users.noreply.github.com>
2022-03-20 06:19:24 +0700

    Fix banned page (#87)

--- draplus/src/pages/BannedPage.tsx
+++ draplus/src/pages/BannedPage.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 interface BannedPageProps {}
 
 const BannedPage: React.FC<BannedPageProps> = () => {
     const location: any = useLocation();
-
+    let navigate = useNavigate();
     if (!location.state?.isBanned) {
         return <Navigate to="/" />;
     }
@@ -16,25 +16,37 @@ const BannedPage: React.FC<BannedPageProps> = () => {
             style={{ backgroundColor: "var(--bg)" }}
         >
             <div
-                className="h-1/2 w-1/4 flex flex-col items-center justify-center border-rounded app-shadow"
+                className="h-1/2 w-1/4 flex flex-col items-center justify-around border-rounded app-shadow"
                 style={{
                     backgroundColor: "var(--element-bg)",
                     minWidth: "350px",
                     maxWidth: "500px",
                 }}
             >
-                <div className="h-auto my-14 mx-2 content-center text-center flex flex-col justify-between items-center">
-                    <p className="text-5xl text-red-600 font-bold">
+                <div className="basis-2/5 content-center text-center flex flex-col justify-center items-center">
+                    <p className="text-3xl text-red-600 font-bold">
                         You are banned
                     </p>
                 </div>
-                <div className="mx-2 mt-14 content-center text-center">
-                    <p className="text-2xl text-white">
+
+                <div className="basis-3/5 flex flex-col items-center justify-start">
+                    <p className="text text-white">
                         Please contact our team at <br />
-                        <a href="mailto:draplus.info@gmail.com?subject=Please unban user">
+                        <a
+                            className="font-bold"
+                            href="mailto:draplus.info@gmail.com?subject=Please unban user"
+                        >
                             draplus.info@gmail.com
                         </a>
                     </p>
+                    <button
+                        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+                        onClick={() => {
+                            navigate("/");
+                        }}
+                    >
+                        Back to Login Page
+                    </button>
                 </div>
             </div>
         </div>
